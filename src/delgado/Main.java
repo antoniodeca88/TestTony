6e55c1b5443095db02db1d745df8940f1f179ce0
ejgdr <84037192+ejgdr@users.noreply.github.com>
2021-05-21 07:39:04 -0600

    Addition of a line to undo something for your stretch challenge Tony

--- src/delgado/Main.java
+++ src/delgado/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
         System.out.println("Hello my name is Tonny");
         System.out.println("Hello Tony, my name is Ema");
+        System.out.println("This is a line you maybe can erase in your stretch challenge");
     }
 }

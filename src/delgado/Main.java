445704b0d2fb7db079b18a8ad1117858f41d89c5
ejgdr <84037192+ejgdr@users.noreply.github.com>
2021-05-21 07:25:51 -0600

    Addition of my name on it, I created the configuration to "Application" to make it run

--- src/delgado/Main.java
+++ src/delgado/Main.java
@@ -3,6 +3,8 @@ package delgado;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello my name is Tonny");
+        System.out.println("Hello Tony, my name is Ema");
     }
 }

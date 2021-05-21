d167be077812d33217079c1fae6cc27a9e3ffa58
anton <antoniodeca88@gamil.com>
2021-05-21 00:27:55 -0500

    EsteEsElBueno

--- src/delgado/Main.java
+++ src/delgado/Main.java
@@ -3,6 +3,6 @@ package delgado;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello my name si Tonny");
+        System.out.println("Hello my name is Tonny");
     }
 }
